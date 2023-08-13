a26194bdaf4c0a77e38c61f3fae10f73ba40d950
chiubowen <cbh@cameo.tw>
2023-08-14 00:29:19 +0800

    ok

--- botrun_pdf_to_text.py
+++ botrun_pdf_to_text.py
@@ -1,40 +1,29 @@
+# from botrun_pdf_to_text import *
 import os
-from pypdf import PdfReader
 from typing import List
+
+from pypdf import PdfReader
 
 
 def convert_pdf_to_txt(file_path: str) -> None:
     base_name, _ = os.path.splitext(file_path)
-
-    # Check if directory for the PDF exists and if not, create one
     output_dir = base_name
     os.makedirs(output_dir, exist_ok=True)
 
     reader = PdfReader(file_path)
     if all(os.path.exists(os.path.join(output_dir, f"{os.path.basename(base_name)}_page_{index + 1}.txt"))
            for index in range(len(reader.pages))):
-        print(f"Skipped (already converted): {file_path}")
         return
 
     try:
-        print(f"Converting: {file_path}")
-
         for index, page in enumerate(reader.pages):
             txt_file_path = os.path.join(output_dir, f"{os.path.basename(base_name)}_page_{index + 1}.txt")
-
-            # Skip if the txt file for this page already exists
             if os.path.exists(txt_file_path):
-                print(f"Skipped (page already converted): {txt_file_path}")
                 continue
-
             txt_content = page.extract_text()
             if txt_content:
                 with open(txt_file_path, "w", encoding="utf-8") as text_file:
                     text_file.write(txt_content)
-                print(f"Saved: {txt_file_path}")
-            else:
-                print(f"No text content in page {index + 1} of {file_path}")
-
     except Exception as e:
         print(f"Error converting {file_path}. Reason: {e}")
 
--- sh/to_pip.sh
+++ sh/to_pip.sh
@@ -1,2 +1,2 @@
 source venv/bin/activate
-python -m to_pip -n botrun-pdf-to-text -v 1.1.0 botrun_pdf_to_text.py
+python -m to_pip -n botrun-pdf-to-text -v 1.1.1 botrun_pdf_to_text.py
